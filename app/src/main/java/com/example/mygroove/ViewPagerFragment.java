384e2e543c3b92ae18e0fc301d82ec035ae7fa9d
Neet Shah <neetshah02@gmail.com>
2018-07-11 20:31:30 -0400

    Added 3 sample screen for the viewpager

--- app/src/main/java/com/example/mygroove/ViewPagerFragment.java
+++ app/src/main/java/com/example/mygroove/ViewPagerFragment.java
@@ -1,24 +1,33 @@
 package com.example.mygroove;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 public class ViewPagerFragment extends Fragment {
 
+    private static final int NUM_PAGES = 3;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
+    
     public ViewPagerFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_view_pager, container, false);
+        return rootview;
     }
 
 }

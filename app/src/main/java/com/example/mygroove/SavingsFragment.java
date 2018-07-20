8d8b0967b0310a8e85cf028f59a1d0cb0a62ea4f
Neet Shah <neetshah02@gmail.com>
2018-07-19 22:48:32 -0400

    Get individual transaction data and map by months

--- app/src/main/java/com/example/mygroove/SavingsFragment.java
+++ app/src/main/java/com/example/mygroove/SavingsFragment.java
@@ -13,10 +13,17 @@ import com.android.volley.VolleyError;
 import com.example.mygroove.MainActivity;
 import com.td.virtualbank.VirtualBankCustomer;
 import com.td.virtualbank.VirtualBankGetCustomerRequest;
+import com.td.virtualbank.VirtualBankGetCustomerTransactionsRequest;
+import com.td.virtualbank.VirtualBankTransaction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressLint("ValidFragment")
 public class SavingsFragment extends Fragment {
     private Context context;
+
     public SavingsFragment(Context context) {
         this.context = context;
         gettransactions();
@@ -41,18 +48,31 @@ public class SavingsFragment extends Fragment {
 
     public void gettransactions() {
 
-        Log.d("TAG", "CHECK ME: "+context);
-        MainActivity.vb.getCustomer(context, "cf41f149-e9f4-4ec0-9d97-c0753d10d4fa_46ecc00a-84f5-4b64-a1fa-4354edeba8c4", new VirtualBankGetCustomerRequest() {
+        Log.d("TAG", "CHECK ME: " + context);
+        MainActivity.vb.getCustomerTransactions(context, "cf41f149-e9f4-4ec0-9d97-c0753d10d4fa_46ecc00a-84f5-4b64-a1fa-4354edeba8c4", new VirtualBankGetCustomerTransactionsRequest() {
             @Override
-            public void onSuccess(VirtualBankCustomer response) {
-                Log.d("TAGG", "CHECK ME: "+response);
-                Log.d("TAGG", "CHECK ME: "+response.getAge());
-
+            public void onSuccess(ArrayList<VirtualBankTransaction> response) {
+                Log.d("TAGGGGG", "CHECK MEEE: " + response);
+                HashMap<String, Double> transactions = new HashMap<>();
+                for (VirtualBankTransaction virtualBankTransaction : response) {
+                    Double amount = virtualBankTransaction.currencyAmount;
+                    Log.d("TAG", "Amount: "+amount);
+                    Double temp;
+                    if (transactions.get(virtualBankTransaction.postDate.substring(0, 7)) == null)
+                        transactions.put(virtualBankTransaction.postDate.substring(0, 7), amount);
+                    else {
+                        temp = Double.valueOf(Math.round(transactions.get(virtualBankTransaction.postDate.substring(0, 7)) + amount));
+                        transactions.put(virtualBankTransaction.postDate.substring(0, 7), temp);
+                    }
+                }
+                for (Map.Entry<String, Double> entry : transactions.entrySet()) {
+                    Log.d("TAG", "LOOOL: " + entry.getKey() + " " + entry.getValue());
+                }
             }
 
             @Override
             public void onError(VolleyError error) {
-                // handle the error
+
             }
         });
     }
